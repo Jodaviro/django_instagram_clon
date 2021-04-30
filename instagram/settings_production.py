b57cb31f157892b14aa47b7bce051e8dea71311f
jodaviro <josedavid_0212@hotmail.com>
2021-05-01 01:22:46 +0200

    last try for today

--- instagram/settings_production.py
+++ instagram/settings_production.py
@@ -4,6 +4,6 @@ from decouple import config
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=config('postgres://lckpwgeahycdul:41df89bc3184750963b3ab53acf5f403358657c96ef63e8e1b51bce1fad7085a@ec2-52-21-252-142.compute-1.amazonaws.com:5432/dqtej0v8j4ole')
+        default=config('DATABASE_URL')
     )
 }
