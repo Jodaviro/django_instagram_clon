e0de2a1cd672713c3828af786444bdcfecd4fcd0
jodaviro <josedavid_0212@hotmail.com>
2021-05-01 17:15:24 +0200

    cloudinary config

--- instagram/settings.py
+++ instagram/settings.py
@@ -127,9 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-CLOUDINARY_URL = config('CLOUDINARY_URL')
 
-DEFAULT_FILE_STORAGE='cloudinary_storage.storage.MediaCloudinaryStorage'
 
 STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
@@ -151,7 +149,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL= '/media/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+CLOUDINARY_URL = config('CLOUDINARY_URL')
 
+DEFAULT_FILE_STORAGE='cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Activate Django-Heroku.heroku logs --tail
 django_heroku.settings(locals())
