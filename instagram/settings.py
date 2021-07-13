f2ba6348f0a9f650ef04c9cef774c8cb00290141
jodaviro <josedavid_0212@hotmail.com>
2021-07-13 15:02:11 +0200

    settings fixed

--- instagram/settings.py
+++ instagram/settings.py
@@ -163,36 +163,7 @@ CLOUDINARY_URL = config('CLOUDINARY_URL')
 DEFAULT_FILE_STORAGE='cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Activate Django-Heroku.heroku logs --tail
-django_heroku.settings(locals())
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        },
-        'console':{
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
+    django_heroku.settings(locals())
 
 
 if config('DJANGO_PRODUCTION', default=False, cast=bool):
